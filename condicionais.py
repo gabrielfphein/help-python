cd310c1819942981b615df74d7143f221e8bfa20
Gabriel Hein <gabrielfphein@hotmail.com>
2023-08-02 15:12:26 -0300

    condicionais.py

---
+++ condicionais.py
@@ -0,0 +1,25 @@
+
+#estrutruras condicionais
+
+idade = 19
+
+if idade >= 18:
+    print('voce é maior de idade.')
+
+
+
+else:
+    print('voce é menor de idade.')
+##
+
+media = float(input('informe a media do estudante'))
+presenca = 100
+if media >= 7 and presenca >= 70:
+    print('voce foi aprovado')
+elif media >= 5:
+    print('recuperação')
+else:
+
+  print('voce foi reprovado')
+
+  
